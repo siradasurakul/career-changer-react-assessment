c657e830f26f4a2d788d37ab7cfe398fefdcb987
sirada <sirada.taan@gmail.com>
2024-01-09 22:30:08 +0700

    submit 2

--- src/Owner.jsx
+++ src/Owner.jsx
@@ -3,10 +3,13 @@ import Layout from "./layout";
 const Owner = () => {
   return (
     <Layout>
+      <div style={{textAlign:'center'}}>
       <h1>Taan - Group D - 23</h1>
-      <img src="../asset/samoyed.jpg" alt="" height="300px" />
+      <img src="https://image.dogilike.com/shareimg/contentimg/2013/prig/Breed/Samoyed-2.jpg" alt="" height="300px" />
       <h2>Short Biography</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+     
     </Layout>
   );
 };
